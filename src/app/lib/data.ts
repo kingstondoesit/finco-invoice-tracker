5c224ba3ef67fd6f4af53a97f1f877b7cf04942c
kingstondoesit <kingstondoesitall@gmail.com>
2024-12-12 10:00:11 +0100

    fetch invoice data and render customer invoice detail on dashboard page

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,6 +1,8 @@
 import {
+  LatestInvoiceRaw,
   Revenue,
 } from './definitions';
+import { formatCurrency } from './utils';
 import dotenv from 'dotenv';
 import pkg from 'pg';
 const { Client } = pkg;
@@ -14,23 +16,40 @@ const client = new Client({
   },
 });
 
+// Connect to the database outside of the functions  
+(async () => {
+  await client.connect();
+})().catch(error => console.error('Connection Error:', error));
+
 export async function fetchRevenue() {
   try {
-    // Connect to the database
-    await client.connect();
-
-    // Fetch data from the revenue table
+    // Fetch data from the revenue table  
     const data = await client.query<Revenue>('SELECT * FROM revenue');
-
-    console.log('Data fetched successfully 🟢');
-
-    // Return the rows
+    console.log('Revenue Data fetched successfully 🟢');
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error('Failed to fetch revenue data. 🔴');
-  } finally {
-    // Ensure the database connection is closed
-    await client.end();
+    throw new Error('Failed to fetch revenue data 🔴');
   }
 }
+
+export async function fetchLatestInvoices() {
+  try {
+    const data = await client.query<LatestInvoiceRaw>(
+      `SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id   
+       FROM invoices   
+       JOIN customers ON invoices.customer_id = customers.id   
+       ORDER BY invoices.date DESC LIMIT 5;`
+    );
+
+    const latestInvoices = data.rows.map((invoice) => ({
+      ...invoice,
+      amount: formatCurrency(invoice.amount),
+    }));
+    console.log('Latest Invoices fetched successfully 🟢');
+    return latestInvoices;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch the latest invoices 🔴');
+  }
+}  
