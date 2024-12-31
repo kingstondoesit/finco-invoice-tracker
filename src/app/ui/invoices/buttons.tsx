082198030c9fb8b3bded5a0a22e89c8c4d516f63
kingstondoesit <kingstondoesitall@gmail.com>
2024-12-31 09:18:14 +0100

    refactor: remove console log from delete invoice button

--- src/app/ui/invoices/buttons.tsx
+++ src/app/ui/invoices/buttons.tsx
@@ -30,7 +30,6 @@ export function DeleteInvoice({ id }: { id: string }) {
       <button
         type="button"
         className="rounded-md border p-2 hover:bg-gray-100"
-        onClick={() => console.log('Delete Invoice', id)}
       >
         <TrashIcon className="w-5" />
       </button>
