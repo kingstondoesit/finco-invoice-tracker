6b58c651963cb25f4b63aaace08bdf1f360d1b03
kingstondoesit <kingstondoesitall@gmail.com>
2025-01-11 14:45:26 +0100

    Added `zod` (version 3.24.1) to project to validate invoice create form data

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -1,13 +1,26 @@
 'use server';
+import { z } from 'zod';
  
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+ 
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
-        customer_id: formData.get('customerId') as string,
-        amount: formData.get('amount') as string,
-        status: formData.get('status') as string,
-    }
-
-    console.log('rawFormData:', rawFormData);
-
-    return rawFormData;
+    const { customerId, amount, status } = CreateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+    console.log('Invoice created successfully 🟢');
+    return { customerId, amountInCents, status, date };
 }
+
+  
