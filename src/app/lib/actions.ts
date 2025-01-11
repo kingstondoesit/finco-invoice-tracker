447c9d3c93295d16b782155a1fa4ed0b70683bbc
kingstondoesit <kingstondoesitall@gmail.com>
2025-01-11 23:50:46 +0100

    refactor: update createInvoice server action function to handle database insertion

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -1,26 +1,46 @@
 'use server';
+
 import { z } from 'zod';
- 
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { client } from './data';
+
+(async () => {
+  await client.connect();
+})().catch(error => console.error('Connection Error:', error));
+
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
+  customer_Id: z.string(),
   amount: z.coerce.number(),
   status: z.enum(['pending', 'paid']),
   date: z.string(),
 });
- 
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
-      customerId: formData.get('customerId'),
-      amount: formData.get('amount'),
-      status: formData.get('status'),
-    });
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
-    console.log('Invoice created successfully 🟢');
-    return { customerId, amountInCents, status, date };
-}
+  try {
+  const { customer_Id, amount, status } = CreateInvoice.parse({
+    customer_Id: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
 
+  await client.query(
+    `INSERT INTO invoices (customer_id, amount, status, date)
+     VALUES ($1, $2, $3, $4)`,
+    [customer_Id, amountInCents, status, date]
+  );
   
+  console.log('Invoice created successfully 🟢');
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+  
+} catch (error) {
+  console.error('Error creating invoice:', error);
+}
+}
