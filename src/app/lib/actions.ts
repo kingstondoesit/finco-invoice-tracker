6c33e195d34b8cd17913bc1d0c79f9ef92c4cb8c
kingstondoesit <kingstondoesitall@gmail.com>
2025-01-13 14:30:23 +0100

    feat: add revenue update functionality. call function after every data mutation action on server

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -18,31 +18,33 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 export async function createInvoice(formData: FormData) {
   const client = await pool.connect();
   try {
-  const { customer_Id, amount, status } = CreateInvoice.parse({
-    customer_Id: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
-  const amountInCents = amount * 100;
-  const date = new Date().toISOString().split('T')[0];
+    const { customer_Id, amount, status } = CreateInvoice.parse({
+      customer_Id: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
 
-  await client.query(
-    `INSERT INTO invoices (customer_id, amount, status, date)
+    await client.query(
+      `INSERT INTO invoices (customer_id, amount, status, date)
      VALUES ($1, $2, $3, $4)`,
-    [customer_Id, amountInCents, status, date]
-  );
-  
-  console.log('Invoice created successfully 🟢');
+      [customer_Id, amountInCents, status, date]
+    );
 
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
-  
-} catch (error) {
-  console.error('Error creating invoice:', error);
+    console.log('Invoice created successfully 🟢');
 
-} finally {
-  client.release();
-}
+    await updateRevenue();
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+
+  } catch (error) {
+    console.error('Error creating invoice:', error);
+
+  } finally {
+    client.release();
+  }
 }
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
@@ -66,14 +68,16 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     console.log('Invoice updated successfully 🟢');
 
+    await updateRevenue();
+
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
   } catch (error) {
     console.error('Error updating invoice:', error);
 
   } finally {
-  client.release();
-}
+    client.release();
+  }
 }
 
 export async function deleteInvoice(id: string) {
@@ -87,10 +91,42 @@ export async function deleteInvoice(id: string) {
 
     console.log('Invoice deleted successfully 🟢');
 
+    await updateRevenue();
+
     revalidatePath('/dashboard/invoices');
 
   } catch (error) {
     console.error('Error deleting invoice:', error);
+  } finally {
+    client.release();
+  }
+}
+
+export async function updateRevenue() {
+  const client = await pool.connect();
+  try {
+    // Delete the existing revenue data
+    await client.query(`DELETE FROM revenue`);
+
+    // Insert new revenue data
+    await client.query(`  
+      INSERT INTO revenue (month, revenue)  
+    SELECT   
+      TO_CHAR(date::DATE, 'Mon') AS month,  
+      SUM(amount) AS revenue  
+    FROM   
+      invoices  
+    WHERE   
+      status = 'paid'  
+    GROUP BY   
+      TO_CHAR(date::DATE, 'Mon')  
+    ORDER BY   
+      MIN(date::DATE);  
+  `);
+
+    console.log("Revenue updated 🟢");
+  } catch (error) {
+    console.error('Error updating revenue:', error);
   } finally {
     client.release();
   }
