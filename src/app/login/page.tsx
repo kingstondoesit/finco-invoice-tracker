b03c790782839412f56b37b1be8c1bcd39e00fc0
kingstondoesit <kingstondoesitall@gmail.com>
2025-01-14 11:55:31 +0100

    create user login page and update sidenav logout link with Kinde Logout component

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { Button } from '@/app/ui/button';
+
+export default function LoginPage() {
+    return (
+        <main className="h-dvh flex flex-col items-center gap-6 text-4xl p-4">
+            <h1>Welcome to Acme Corp</h1>
+            <Button type="submit">
+                <LoginLink>Sign in</LoginLink>
+            </Button>
+        </main>
+
+    )
+}
