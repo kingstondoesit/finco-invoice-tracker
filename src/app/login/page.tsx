36268db5d1d2e576826d934ca56b1ac02f6d7d1d
kingstondoesit <kingstondoesitall@gmail.com>
2025-01-15 01:54:59 +0100

    style login page, extend bg image in tailwind config

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,24 @@
 import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { Button } from '@/app/ui/button';
+import { lusitana } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: {
+        template: "%s | Finance Company Invoice Tracker",
+        default: "Login",
+    },
+}
 
 export default function LoginPage() {
     return (
-        <main className="h-dvh flex flex-col items-center gap-6 text-4xl p-4">
-            <h1>Welcome to Acme Corp</h1>
-            <Button type="submit">
-                <LoginLink>Sign in</LoginLink>
-            </Button>
+        <main className={`${lusitana.className} antialiased bg-login-img h-dvh m-auto flex flex-col justify-center items-center gap-6 text-3xl sm:text-4xl p-4`}>
+            <div className="bg-white flex flex-col items-center border-2 gap-5 m-2 p-8 sm:p-16 border-gray-100 shadow-lg">
+                <h1>Welcome to Acme Corp</h1>
+                <Button type="submit" className="font-serif">
+                    <LoginLink>Sign in</LoginLink>
+                </Button>
+            </div>
         </main>
 
     )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'login-img': 'url("/images/hero-mobile.png")',
+      },
       gridTemplateColumns: {
         13: 'repeat(13, minmax(0, 1fr))'
       },
